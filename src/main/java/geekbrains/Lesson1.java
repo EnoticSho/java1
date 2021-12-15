4a09fb04d8fa3efc1537e22d7d09f53a79873055
sergey turpakov <paradise666hell@mail.ru>
2021-12-15 22:47:00 +0300

    homework1 done

--- src/main/java/geekbrains/Lesson1.java
+++ src/main/java/geekbrains/Lesson1.java
@@ -2,6 +2,36 @@ package geekbrains;
 
 public class Lesson1 {
     public static void main(String[] args) {
-        
+        printThreeWords();
+        printColor();
+        checkSumSign();
+        compareNumbers();
+    }
+
+    public static void printThreeWords(){
+        System.out.println("Orange\n" +
+                "Banana\n" +
+                "Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 5;
+        int b = 2;
+        if (a + b >= 0) System.out.println("Сумма положительная");
+        else System.out.println("Сумма отрицательная");
+    }
+
+    public static void printColor(){
+        int value = 101;
+        if ( value <= 0) System.out.println("Красный");
+        else if ( value > 0 && value <= 100 ) System.out.println("Жёлтый");
+        else System.out.println("Зелёный");
+    }
+
+    public static void compareNumbers(){
+        int a = 2;
+        int b = 1;
+        if ( a >= b ) System.out.println("a >= b");
+        else System.out.println("a < b");
     }
 }
