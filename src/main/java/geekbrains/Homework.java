095f9888a7a061c51d6bec5e27045a17ceba8516
sergey turpakov <paradise666hell@mail.ru>
2021-12-22 20:56:03 +0300

    Homework3 done

--- src/main/java/geekbrains/Homework.java
+++ src/main/java/geekbrains/Homework.java
@@ -4,10 +4,10 @@ public class Homework {
     public static void main(String[] args) {
         // first task
         int[] array1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i < array1.length; i++){
-            if ( array1[i] == 1){
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] == 1) {
                 array1[i] = 0;
-            }else array1[i] = 1;
+            } else array1[i] = 1;
         }
 
         System.out.println("first task");
@@ -18,7 +18,7 @@ public class Homework {
 
         // second task
         int[] array2 = new int[100];
-        for (int i = 0; i < array2.length; i++){
+        for (int i = 0; i < array2.length; i++) {
             array2[i] = i + 1;
         }
 
@@ -30,8 +30,8 @@ public class Homework {
 
         // third task
         int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < array3.length; i++){
-            if ( array3[i] < 6){
+        for (int i = 0; i < array3.length; i++) {
+            if (array3[i] < 6) {
                 array3[i] *= 2;
             }
         }
@@ -44,7 +44,7 @@ public class Homework {
 
         // fourth task
         int[][] array4 = new int[5][5];
-        for (int i = 0; i < array4.length; i++){
+        for (int i = 0; i < array4.length; i++) {
             array4[i][i] = 1;
             array4[i][array4.length - 1 - i] = 1;
         }
@@ -69,7 +69,7 @@ public class Homework {
         System.out.println("The sixth task");
         int max = array3[0];
         int min = array3[0];
-        for (int i = 1; i < array3.length; i++){
+        for (int i = 1; i < array3.length; i++) {
             max = Math.max(array3[i], max);
             min = Math.min(array3[i], min);
         }
@@ -82,34 +82,34 @@ public class Homework {
 
         //The eighth task
         System.out.println("The eighth task");
-        int [] array8 = {2,4,6,1,6,7} ;
+        int[] array8 = {2, 4, 6, 1, 6, 7};
         array8 = arrayShift(array8, 3);
-        for (int i : array8){
+        for (int i : array8) {
             System.out.print(i + " ");
         }
 
     }
 
     //The fifth task
-    public static int[] arrayInitializing(int len, int initialValue){
+    public static int[] arrayInitializing(int len, int initialValue) {
         int[] array = new int[len];
-        for ( int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = initialValue;
         }
         return array;
     }
 
     //The seventh task
-    public static boolean checkBalance(int[] array){
+    public static boolean checkBalance(int[] array) {
         int sumStart = 0;
         int sumEnd = 0;
         boolean a = false;
-        for ( int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             sumStart += array[i];
-            for ( int j = array.length - 1; j > i; j--){
+            for (int j = array.length - 1; j > i; j--) {
                 sumEnd += array[j];
             }
-            if ( sumStart == sumEnd){
+            if (sumStart == sumEnd) {
                 a = true;
                 break;
             }
@@ -119,7 +119,7 @@ public class Homework {
     }
 
     //The eighth task
-    public static int[] arrayShift(int[] array, int n){
+    public static int[] arrayShift(int[] array, int n) {
         if (n > 0) {
             for (int i = 0; i < n; i++) {
                 int a = array[0];
@@ -131,9 +131,9 @@ public class Homework {
                     array[j] = array[j + 1];
                 }
             }
-        }else {
+        } else {
             for (int i = 0; i < Math.abs(n); i++) {
-                int a = array[array.length-1];
+                int a = array[array.length - 1];
                 for (int j = array.length - 1; j >= 0; j--) {
                     if (j == 0) {
                         array[j] = a;
